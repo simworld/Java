15507f636bca709a165043e88c332f366fe71422
Ugoivy <52260589+Ugoivy@users.noreply.github.com>
2019-11-27 18:59:58 +0000

    GUI separations

--- src/org/leonardi/bugreport/GUI/FirstWindow.java
+++ src/org/leonardi/bugreport/GUI/FirstWindow.java
@@ -9,6 +9,8 @@ public class FirstWindow extends javax.swing.JFrame {
         initComponents();
     }
 
+
+
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
